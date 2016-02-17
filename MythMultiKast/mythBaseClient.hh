6f9f67e807eaf2b765ec1785554fbfcff7d18c7e
mythkast <mythkast@c8ac6785-c235-7648-bd35-15bfb6c4594e>
2016-02-17 02:01:22 +0000

    git-svn-id: https://svn.mythkast.net/svn/mythmultikast/trunk@88 c8ac6785-c235-7648-bd35-15bfb6c4594e

--- MythMultiKast/mythBaseClient.hh
+++ MythMultiKast/mythBaseClient.hh
@@ -4,9 +4,6 @@ extern "C"{
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#ifdef WIN32
-//	#include <Windows.h>
-//#endif
 #include <time.h>
 };
 #include "MythSocket.hh"
@@ -15,7 +12,6 @@ class mythBaseClient:
 	public mythAvlist
 {
 public:
-	//static mythBaseClient* CreateNew(mythStreamServer* server , PEOPLE* people);
 	static int SendThreadStatic(void* data){
 		mythBaseClient* cli = (mythBaseClient*) data;
 		if (cli)
@@ -25,34 +21,19 @@ public:
 	}
 	int SendThread();
 	static mythBaseClient* CreateNew(MythSocket* people, bool usethread = false);
-	//static int mainthreadstatic(void* data);
-	//mythStreamServer* getServer();	//removed
-	//int changeStreamServer(mythStreamServer* server);
-	//int mainthread();
-	//int start();
-	//int stop();
-	//int SetTop(char* data,int length);
 	int mythSendMessage(void* data, int length = -1);
 	int DataCallBack(void* data, int len);
 	~mythBaseClient(void);
-	//int active;
 private:
 	bool musethread;		//use thread or not
 	bool isfirst;
 	MythSocket* mpeople;
 	int isrunning;
 	SDL_Thread* mainthreadhandle;
-	//int mypacketlength;
-	//char* mypacketdata;
-	//int tmppacketlength;
-	//char* tmppacketdata;
 	SDL_mutex* mymutex;
 	int generate(char* data,int length);
 	int iFrameCount;
-	//int GetTop();
 protected:
-	//mythStreamServer* mserver;
-	//mythBaseClient(mythStreamServer* server , PEOPLE* people);
 	mythBaseClient(MythSocket* people, bool usethread);
 	SDL_Thread* mthread;
 	bool misrunning;
