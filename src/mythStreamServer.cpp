7ac8a5f58c69013a8f50648a63aad12c7c30d1df
godka <asdic182@sina.com>
2016-12-06 15:33:10 +0800

    streamserver换源

--- src/mythStreamServer.cpp
+++ src/mythStreamServer.cpp
@@ -90,7 +90,7 @@ void mythStreamServer::connect()
 				delete result;
 			}
 			else{
-				this->decoder = mythStreamDecoder::CreateNew("192.168.1.130", 1017);
+				this->decoder = mythStreamDecoder::CreateNew("120.204.70.218", 1017);
 				//this->decoder = mythLive555Decoder::CreateNew("rtsp://192.168.31.128:554/tcp/av0_0", "admin", "888888");
 				if (decoder){
 					decoder->SetMagic((void*) m_cameraid);	//set magic
